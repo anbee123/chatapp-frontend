3da28ef615be83478e556d27afa807e15e745b91
An Bui <anhipchipchip@gmail.com>
2023-02-22 02:49:22 -0500

    implemented routing

--- src/App.js
+++ src/App.js
@@ -3,40 +3,17 @@ import './App.css';
 import { testGetAll } from './api'
 import { useEffect, useState } from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import ChatPage from './pages/Chat';
+import HomePage from './pages/Home';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false)
-  useEffect(() => {
-    setIsLoading(true)
-    const getAllData = async () => {
-      const response = await testGetAll()
-      console.log({response})
-      setIsLoading(false)
-    }
-    getAllData()
-  }, [])
-  
-  const handleClick = async () => {
-    setIsLoading(true)
-    const response = await testGetAll()
-    console.log('button clicked', {response})
-    setIsLoading(false)
-  }
-  const handleClick111 = async () => {
-    await fetch(`http://localhost:8000/api/chat`, {method: 'GET'})
-      .then(response => response.json())
-      .then(data => {
-        console.log('api fetched', {data})
-      })
-  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={handleClick}>Test Api call</button>
-        <button onClick={handleClick111}>Test Fetch call</button>
-        
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<HomePage />} path='/' />
+        <Route element={<ChatPage />} path='/chat' />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
