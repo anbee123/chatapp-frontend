cd3a91e305e500abe4d7732e809a6cdf02428e30
An Bui <anhipchipchip@gmail.com>
2023-02-22 03:28:22 -0500

    outlined for chatpage

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
 
 const ChatPage = () => {
     return <div>
         This is Chat Page
+
+        <div>
+            <Link to='/'>
+                Back to Homepage
+            </Link>
+        </div>
+
+        <div>
+            The messages here
+            
+            <button>Send message</button>
+        </div>
     </div>
 }
 export default ChatPage
