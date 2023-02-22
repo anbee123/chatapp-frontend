5476b40e5fa411e54be2cafa38311dab3e88032e
An Bui <anhipchipchip@gmail.com>
2023-02-22 03:40:07 -0500

    fetched message data

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,8 +1,24 @@
+import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import AppContext from "../context"
 
 const ChatPage = () => {
+    const { userName, roomName } = useContext(AppContext)
+    const [ messages, setMessages ] = useState([])
+
+    useEffect(() => {
+        const fetchMessages = async () => {
+            await fetch(`http://localhost:8000/api/chat/${roomName}/${userName}`, {method: 'GET'})
+                .then(response => response.json())
+                .then(data => setMessages(data))
+                .catch(err => console.log(err))
+        }
+        fetchMessages()
+    }, [])
     return <div>
-        This is Chat Page
+        <h1>This is Chat Page</h1>
+        <p>Room Name : {roomName}</p>
+        <p>User Name : {userName}</p>
 
         <div>
             <Link to='/'>
@@ -12,7 +28,7 @@ const ChatPage = () => {
 
         <div>
             The messages here
-            
+
             <button>Send message</button>
         </div>
     </div>
