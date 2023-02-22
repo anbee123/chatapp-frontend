6079c95c23f401f9719b8078c5e0910c0ccaddec
An Bui <anhipchipchip@gmail.com>
2023-02-22 03:08:15 -0500

    switched from chat.js to home.js

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,34 +1,7 @@
-import { useState } from 'react'
 
 const ChatPage = () => {
-    const [userName, setUserName] = useState('')
-    const [roomName, setRoomName] = useState('')
-
-    const onRoomCreate = () => {
-        console.log('inputed values: ', {userName, roomName})
-    }
-    
     return <div>
         This is Chat Page
-        <div>
-            <label htmlFor='room'>Enter the Room name</label>
-            <input
-                type='text'
-                name='room'
-                placeholder='Room name' 
-                value={roomName}
-                onChange={e => setRoomName(e.target.value)}
-            />
-            <label htmlFor='user'>Enter the User name</label>
-            <input
-                type='text'
-                name='user'
-                placeholder='User name' 
-                value={userName}
-                onChange={e => setUserName(e.target.value)}
-            />
-            <button onClick={onRoomCreate}>Enter Room</button>
-        </div>
     </div>
 }
 export default ChatPage
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,34 @@
+import { useState } from "react"
+
 const HomePage = () => {
+    const [userName, setUserName] = useState('')
+    const [roomName, setRoomName] = useState('')
+
+    const onRoomCreate = () => {
+        console.log('inputed values: ', {userName, roomName})
+    }
+    
     return <div>
         This is Home Page
+        <div>
+            <label htmlFor='room'>Enter the Room name</label>
+            <input
+                type='text'
+                name='room'
+                placeholder='Room name' 
+                value={roomName}
+                onChange={e => setRoomName(e.target.value)}
+            />
+            <label htmlFor='user'>Enter the User name</label>
+            <input
+                type='text'
+                name='user'
+                placeholder='User name' 
+                value={userName}
+                onChange={e => setUserName(e.target.value)}
+            />
+            <button onClick={onRoomCreate}>Enter Room</button>
+        </div>
     </div>
 }
 export default HomePage
