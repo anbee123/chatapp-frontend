12858ce8b83d5510095c436f0d3ee5481b58b775
An Bui <anhipchipchip@gmail.com>
2023-02-22 20:45:27 -0500

    deploying

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 
 import './App.css';
-import { testGetAll } from './api'
-import { useEffect, useState } from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import ChatPage from './pages/Chat';
 import HomePage from './pages/Home';
